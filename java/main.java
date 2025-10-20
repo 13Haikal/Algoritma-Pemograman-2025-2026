f8cca5e4b8404752d6a27add82ee6e45a3c1c670
13Haikal <13haikalmochammmad@gmail.com>
2025-10-20 17:35:22 +0700

    python dan java
    tipe data, variabel dan konstanta

--- java/main.java
+++ java/main.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main {
+public class main {
     public static void main(String[]args) {
         final double PI= 3.14159;
         Scanner sc= new Scanner(System.in);
