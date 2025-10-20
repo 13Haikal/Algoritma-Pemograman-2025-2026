8cafb99175933a2d77852cd28639a1cf17fc1831
13Haikal <13haikalmochammmad@gmail.com>
2025-10-20 17:40:09 +0700

    latihan2.java
    mencoba

--- java/Latihan2.java
+++ java/Latihan2.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class main {
+public class Latihan2 {
     public static void main(String[]args) {
         final double PI= 3.14159;
         Scanner sc= new Scanner(System.in);
