d7d11e7f93019b10ee207253d505d04948bc6370
13Haikal <13haikalmochammmad@gmail.com>
2026-01-19 09:45:38 +0700

    Soal  UAS

--- Jawabansoaluasno4/SoalUAS4.java
+++ Jawabansoaluasno4/SoalUAS4.java
@@ -3,12 +3,15 @@ public class SoalUAS4 {
         int n = 5;
         int i, j;
 
-        // Bagian yang kamu tulis di VS Code sudah benar:
-for (i = 0; i < n; i++) {
-    for (j = 0; j <= i; j++) {
-        System.out.print(j);
-    }
-    System.out.println();
-}
+        // Pola bergeser ke kiri
+        for (i = 0; i < n; i++) {
+            for (int k = n - i; k > 1; k--) {
+                System.out.print(" ");
+            }
+            for (j = 0; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
     }
 }
