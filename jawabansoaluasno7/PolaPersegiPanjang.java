5357f88ebeadfa3f63dcfa2a99269c2e1e96e05c
13Haikal <13haikalmochammmad@gmail.com>
2026-01-19 10:06:53 +0700

    Soal UAS no 6

---
+++ jawabansoaluasno7/PolaPersegiPanjang.java
@@ -0,0 +1,13 @@
+public class PolaPersegiPanjang {
+    public static void main(String[] args) {
+        int tinggi = 3; // Jumlah baris
+        int lebar = 8;  // Jumlah kolom
+
+        for (int i = 0; i < tinggi; i++) {
+            for (int j = 0; j < lebar; j++) {
+                System.out.print("* ");
+            }
+            System.out.println(); // Pindah baris
+        }
+    }
+}
---
+++ jawabansoaluasno7/polabentukkotak.java
@@ -0,0 +1,14 @@
+
+public class polabentukkotak {
+    public static void main(String[] args) {
+            int n = 5;
+        int i, j;
+
+        for(i = 0; i < 5; i++) {
+            for(j = 0; j < 5; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+}
+}
